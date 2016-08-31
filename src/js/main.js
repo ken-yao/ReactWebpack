d07ee9ec93eaa8ed21609e9bd825d7d36bb2226f
yaoyonstudio <yaoyonstudio@gmail.com>
2016-08-31 18:29:46 +0800

    'init'

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Hello from './components/Hello';
+const app = document.getElementById('app');
+// ReactDOM.render(<Hello name="ken yao" />, app);
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  app
+);
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+//优化1
+process.env.NODE_ENV = 'production';
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+module.exports = {
+	context: path.join(__dirname, "src"),
+	devtool: debug ? "inline-sourcemap" : null,
+	entry: "./js/main.js",
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel-loader',
+				query: {
+	 				presets: ['react', 'es2015']	
+				}
+			}
+		]
+	},
+	output: {
+	path: __dirname + "/src/",
+	filename: "main.min.js"
+	},
+	plugins: debug ? [] : [
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('production')
+			}
+		})
+	],
+};
+//优化2
+//压缩
+//webpack --progress -p
